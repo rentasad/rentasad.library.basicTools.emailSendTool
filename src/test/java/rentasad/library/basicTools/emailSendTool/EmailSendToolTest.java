692329012e209e763df6ff4600ef6c3d25b3e549
Matthias Staud <matthias.staud@gustini.de>
2021-08-30 17:20:51 +0200

    Bugfix Test class

--- src/test/java/rentasad/library/basicTools/emailSendTool/EmailSendToolTest.java
+++ src/test/java/rentasad/library/basicTools/emailSendTool/EmailSendToolTest.java
@@ -29,4 +29,23 @@ public class EmailSendToolTest
         emailSendTool.sendMail(smtpTo, smtpFrom, "UNIT_TEST testSendMailString", "This is a Unit-Test of the Modul rentasad.library.basicTools.emailSendTool EmailSendToolTest");
 //        EmailSendTool.sendeEmail("192.168.111.253",null, null, "it@gustini.de", "matthias.staud@gustini.de,matthias@familie-staud.de", "TEST1-SUBJECT", "TEST1 INHALT");
     }
+    @Test
+    public void testMultipleMail() throws Exception
+    {
+    	final String smtpHost = "192.168.111.72";
+    	final String smtpPort = "25";
+    	final String smtpFrom = "it@gustini.de";
+    	final String smtpTo = "matthias.staud@gustini.de, it@gustini.de";
+    	
+    	
+    	
+    	
+        EmailConfigObject emailConfigObject = new EmailConfigObject(smtpHost);
+        emailConfigObject.setValue(smtpPort, EmailConfigParameterEnum.port);
+        emailConfigObject.setValue(smtpFrom, EmailConfigParameterEnum.from);
+        emailConfigObject.setValue(smtpTo, EmailConfigParameterEnum.to);
+
+        EmailSendTool emailSendTool = new EmailSendTool(emailConfigObject);
+        emailSendTool.sendMail(smtpTo, smtpFrom, "UNIT_TEST testSendMailString", "This is a Unit-Test of the Modul rentasad.library.basicTools.emailSendTool EmailSendToolTest");
+    }
 }
